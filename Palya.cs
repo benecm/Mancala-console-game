9aca68fe34968c1589771f5f5a806047d5b3fc0d
benecm <123589351+benecm@users.noreply.github.com>
2023-01-25 21:16:08 +0100

    Add files via upload

---
+++ Palya.cs
@@ -0,0 +1,390 @@
+﻿using System;
+
+namespace nagyhf1
+{
+    class Palya
+    {
+        public int[] palya;
+
+        public Palya()
+        {
+            palya = new int[12];
+            for(int i = 0; i<palya.Length;i++)
+            {
+                palya[i] = 4;
+            }
+        }
+
+        public void Kiir()
+        {
+            Console.Clear();
+            Console.Write("Gép:\t\t");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(palya[i]);
+                Console.Write("\t");
+            }
+            Console.Write("\n" + "Játékos:\t");
+            for (int j = 11; j >= 6; j--)
+            {
+                Console.Write(palya[j]);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+        }
+
+        public int LepesVegrehajtasJatekos(int n)
+        {
+            int index = 11 - (n - 1);
+            int eredeti = index;
+            int magok = palya[index];
+            palya[index] = 0;
+
+            if (index != 0)
+            {
+                index--;
+            }
+
+            while (magok != 0 && index != 0)
+            {
+                palya[index] += 1;
+                index--;
+                magok--;
+            }
+            if (magok != 0)
+            {
+                if (eredeti != 0)
+                {
+                    palya[0] += 1;
+                }
+                magok--; // ennek lehet inkabb az ifben kellene lennie
+
+                if (magok != 0)
+                {
+                    if ((n - 1) == 0)
+                    {
+                        index = 10;
+                    }
+                    else
+                    {
+                        index = 11;
+                    }
+                    while (magok != 0 && index >= 0)
+                    {
+                        if(index == eredeti)//ez az ifet utolag irtam be a valasztott hely ellenorzesere
+                        {
+                            index--;
+                        }
+                        palya[index] += 1;
+                        magok--;
+                        index--;
+                        if (index == 11 - (n - 1))
+                        {
+                            index--;
+                        }
+                    }
+                }
+            }
+            return index;
+        }
+
+        public int PontellenorzesJatekos(int n)
+        {
+            int pontok = 0;
+            n++;
+            if (n > 5)
+            {
+                return pontok;
+            }
+            else
+            {
+
+                while (n <= 5)
+                {
+
+                    if (palya[n] == 2)
+                    {
+                        pontok += 2;
+                        palya[n] = 0;
+                        n++;
+                    }
+                    else if (palya[n] == 3)
+                    {
+                        pontok += 3;
+                        palya[n] = 0;
+                        n++;
+                    }
+                    else
+                    {
+                        n = 6;
+                    }
+                }
+            }
+            return pontok;
+        }
+
+        public int LepesVegrehajtasGep(int n) //pontos utolsohellyel ter vissza
+        {
+            int index = n;
+            int eredeti = index;
+            int magok = palya[index];
+            palya[index] = 0;
+            if (index != 0)
+            {
+                index--;
+            }
+            else
+            {
+                index = 11;
+            }
+
+
+            while (magok != 0 && index != 0)
+            {
+                if(index == eredeti && eredeti != 0) //eredeti hely ellenorzes
+                {
+                    index--;
+                }
+                palya[index] += 1;
+                magok--;
+
+                index--;
+            }
+            if (magok != 0)
+            {
+                if(eredeti != 0)
+                {
+                    palya[0] += 1;
+                    magok--;
+                }
+                //palya[0] += 1; ezt irtam bele az ujjonan keszult if-be az eredeti hely ellenorzes miatt
+                //magok--;
+                
+                index = 11;
+            }
+            while (index != 0 && magok != 0)
+            {
+                palya[index] += 1;
+                magok--;
+                index--;
+            }
+            if (magok != 0)
+            {
+                palya[0] += 1;
+                magok--;
+                index = 11;
+            }
+            while (index != 0 && magok != 0)
+            {
+                palya[index] += 1;
+                magok--;
+                index--;
+            }
+            return index + 1;
+
+
+        }
+
+        public Palya ProbaLepesVegrehajtasGep(int n) // pontos indexet ker, és visszater egy lelepett palyaval
+        {
+
+            
+            Palya tomb = new Palya();
+            for(int i = 0; i < palya.Length;i++)
+            {
+                tomb.palya[i] = palya[i];
+            }
+
+            int index = n;
+            int magok = tomb.palya[index];
+            tomb.palya[index] = 0;
+            if (index != 0)
+            {
+                index--;
+            }
+
+
+            while (magok != 0 && index != 0)
+            {
+                tomb.palya[index] += 1;
+                magok--;
+
+                index--;
+            }
+            if (magok != 0)
+            {
+                tomb.palya[0] += 1;
+                index = 11;
+            }
+            while (index != 0 && magok != 0)
+            {
+                tomb.palya[index] += 1;
+                magok--;
+                index--;
+            }
+            return tomb;
+        }
+        
+        public int ProbaLepesVegrehajtasIndexGep(int n)//az utolso lepett tomb indexet adja vissza
+        {
+            
+            Palya tomb = new Palya();
+            for (int i = 0; i < palya.Length; i++)
+            {
+                tomb.palya[i] = palya[i];
+            }
+
+            int index = n;
+            int magok = tomb.palya[index];
+            tomb.palya[index] = 0;
+            if (index != 0)
+            {
+                index--;
+            }
+            else
+            {
+                index = 11;
+            }
+
+
+            while (magok != 0 && index != 0)
+            {
+                tomb.palya[index] += 1;
+                magok--;
+
+                index--;
+            }
+            if (magok != 0)
+            {
+                tomb.palya[0] += 1;
+                magok--;
+                index = 11;
+            }
+            while (index != 0 && magok != 0)
+            {
+                tomb.palya[index] += 1;
+                magok--;
+                index--;
+            }
+
+
+            return index + 1;
+        }
+
+        public int PontEllenorzesGepi(int n)//valoban leellnorzi a pontokat, es a megfelelo helyeken 0-ra allit
+        {
+            int pontok = 0;
+
+            if (n < 6)
+            {
+                return pontok;
+            }
+            else
+            {
+
+                while (n <= 11)
+                {
+
+
+                    if (palya[n] == 2)
+                    {
+                        pontok += 2;
+                        palya[n] = 0;
+                        n++;
+                    }
+                    else if (palya[n] == 3)
+                    {
+                        pontok += 3;
+                        palya[n] = 0;
+                        n++;
+                    }
+                    else
+                    {
+                        n = 12;
+                    }
+                }
+            }
+            return pontok;
+        }
+
+        public int GepiProba(int n,Palya eredeti)//kap egy palyat ahol mar probaszeruen volt lépés végrehajtva., visszater a ponttal.
+        {
+            
+            Palya probapalya = new Palya();
+            for (int i = 0; i < palya.Length; i++)
+            {
+                probapalya.palya[i] = eredeti.palya[i];
+            }
+
+            int pontok = 0;
+
+            if (n < 6)
+            {
+                return pontok;
+            }
+            else
+            {
+
+                while (n <= 11)
+                {
+
+                    if (probapalya.palya[n] == 2)
+                    {
+                        pontok += 2;
+                        probapalya.palya[n] = 0;
+                        n++;
+                    }
+                    else if (probapalya.palya[n] == 3)
+                    {
+                        pontok += 3;
+                        probapalya.palya[n] = 0;
+                        n++;
+                    }
+                    else
+                    {
+                        n = 12;
+                    }
+                }
+            }
+            return pontok;
+        }
+
+        public int GepiJatekos()
+        {
+            /*
+             * A gépi játékos megkapja az eredeti pályát, ahol egy ciklusban végig ellenörzi az összes lehetséges
+             * a próba pályával, az utolsó lépés helyével, és leellenörzi a kapott pontokat.
+             * A kapott pontokat egy maxkereséssel eltárolja a hozzátartozó ház-számmal.
+             * Ha nem talált olyan lépést amivel pontot tudna szerezni akkor random választ egy házat, és azzal fog játszani.
+             * Ha van olyan ház ahol pontot tud szerezni akkor azzal fog lépni.
+             * Ha több olyan ház van ahol tud pontot szerezni akkor a legtöbb pontot érő lépést használja, ha több 
+             * ilyen van akkor, az utolsó ilyen házat fogja használni a gép.
+             */
+            Random rnd = new Random();
+            int legjobbpont = 0;
+            int legjobbidx = 0;
+            for (int i = 0; i < 6; i++)
+            {             
+                int probautolsohely = ProbaLepesVegrehajtasIndexGep(i);
+                Palya probapalya = ProbaLepesVegrehajtasGep(i);
+
+                int temp = GepiProba(probautolsohely, probapalya);
+                if (temp > legjobbpont)
+                {
+                    legjobbpont = temp;
+                    legjobbidx = i;
+                }
+            }
+            if (legjobbpont == 0)
+            {
+                legjobbidx = rnd.Next(0, 6);
+                while (palya[legjobbidx] == 0)
+                {
+                    legjobbidx = rnd.Next(0, 6);
+                }
+            }
+            return legjobbidx;
+        }
+
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace nagyhf1
+{
+    class Program
+    {
+        static int HazValasztas()
+        {
+            Console.WriteLine("Melyik házat választod?");
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+        } // kivalasztja a hazat
+
+        static void Main()
+        {
+            
+            Palya map = new Palya();
+            map.Kiir();
+            int JatekosPont = 0;
+            int GepiPont = 0;
+            bool egyenlo = false;
+            while (GepiPont != 25 && JatekosPont != 25)
+            {
+                //A játékos vélasztja ki a házat.
+                int haz = HazValasztas();
+                if (haz > 6)
+                {
+                    while (haz > 6)
+                    {
+                        Console.WriteLine("Kérlek a saját házadat válaszd! (1-től 6-ig)");
+                        haz = HazValasztas();
+                    }
+                    
+                }
+                
+                //játékos lépésének a végrehajtása, majd a pontok leellenőrzése.
+                int utolsohely = map.LepesVegrehajtasJatekos(haz);
+                JatekosPont += map.PontellenorzesJatekos(utolsohely);
+                
+                // a gépi játékosnak átadom a pályát, amivel kiválasztja a megfelelő lépést.
+                int gepivalasztott = map.GepiJatekos();
+
+                // a lépést végrehajta a program, és eltárolja az utolsó házat, ahova került mag.
+                int gepiutolsohely = map.LepesVegrehajtasGep(gepivalasztott);
+
+                //az utolsó lépett ház helyétől visszaszámolja a program a pontokat.
+                GepiPont += map.PontEllenorzesGepi(gepiutolsohely);
+                map.Kiir();
+                Console.WriteLine("A gép által választott ház: " + (gepivalasztott+1));
+
+                Console.WriteLine("Az eredmény:");
+                Console.WriteLine("Játékos: " + JatekosPont);
+                Console.WriteLine("Gép: " + GepiPont);
+                
+                //itt ellenőrzi a program ha egyenlő, azaz döntetlen eredményre jutott a játék.
+                if(GepiPont == 24 && JatekosPont == 24)
+                {
+                    egyenlo = true;
+                    GepiPont = 25;
+                }
+            }
+            Console.Clear();
+
+            if(egyenlo)
+            {
+                Console.WriteLine("A játék véget ért! Az eredmény: Döntetlen.");
+            }
+            else if(GepiPont == 25)
+            {
+                Console.WriteLine("A játék végetért! A gépijátékos nyert!");
+            }
+            else
+            {
+                Console.WriteLine("Gratulálok, megnyerted a játékot!");
+            }
+            Console.ReadKey();           
+        }
+    }
+}
+
+
